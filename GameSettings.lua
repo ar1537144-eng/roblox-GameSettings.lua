377ace37057307e199071580da6cb5150cc3bdc2
ar1537144-eng <ar1537144@students.wlcsd.org>
2025-11-24 18:17:17 -0500

    Add files via upload

---
+++ GameSettings.lua
@@ -0,0 +1,20 @@
+﻿local GameSettings = {}
+
+
+-- Game Variables
+GameSettings.intermissionDuration = 5
+GameSettings.matchDuration = 90
+GameSettings.minimumPlayers = 2
+GameSettings.transitionTime = 5
+
+
+-- Possible ways that the game can end.
+GameSettings.endStates = {
+        TimerUp = "TimerUp",
+        FoundWinner = "FoundWinner"
+}
+
+
+
+
+return GameSettings
